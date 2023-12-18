b5c19cca2c6332c370df271d4f2bad4be50c0b47
gupta4327 <gupta4327@gmail.com>
2023-12-18 21:05:36 +0530

    added data and models in dvc add

---
+++ src/models/hyperparameters.py
@@ -0,0 +1,56 @@
+#importing all necessary libraries
+from hyperopt import hp
+from hyperopt.pyll.base import scope
+import pickle
+from pathlib import Path
+
+#nested dictionary for different hyperparameters of different models
+hyperparameters = {
+    'LinearRegression':
+            {
+            },
+    'DecisionTree':
+            {
+               'max_depth': hp.choice('max_depth', [8,10,15,20 ]),
+               'criterion': hp.choice('criterion', ['ginni', 'entropy']),
+               'min_samples_split': scope.int(hp.uniform('min_Samples_split',300,400)), 
+               'max_features': hp.choice('max_features', ['auto', 'sqrt', 'log2', None])
+            },
+    'RandomForest': 
+           {
+                'n_estimators' : hp.choice('n_estimators', [50,80,120]), 
+                'max_depth': hp.choice('max_depth', [8,10,15,20,30,40 ]), 
+                'max_features': hp.choice('max_features', ['sqrt', 'log2', None]),
+                'min_samples_split': scope.int(hp.uniform('min_Samples_split',200,400)),
+                'max_sample': hp.uniform('max_sample',0.4,0.8)
+           },
+    'GradientBoost':
+            {
+                'n_estimators' : hp.choice('n_estimators', [80,120,180]), 
+                'max_depth': hp.choice('max_depth', [4,8,10,12]), 
+                'max_features': hp.choice('max_features', ['sqrt', 'log2', None]),
+                'min_samples_split': scope.int(hp.uniform('min_Samples_split',1000,1500)),
+                'subsample': hp.uniform('max_sample',0.6,0.8),
+                'learning_rate': hp.uniform('learning_rate',0.01,0.1),
+                'verbose': hp.choice('verbose', [1])
+            },
+    'XtremeGradientBoost':
+            {
+                'n_estimators' : hp.choice('n_estimators', [80,120,180]), 
+                'max_depth': hp.choice('max_depth', [4,8,10,12]), 
+                'max_features': hp.choice('max_features', ['sqrt', 'log2', None]),
+                'min_samples_split': scope.int(hp.uniform('min_Samples_split',1000,1500)),
+                'subsample': hp.uniform('max_sample',0.6,0.8),
+                'learning_rate': hp.uniform('learning_rate',0.03,0.3),      
+                'gamma':hp.uniform('verbose',0.09,0.4),
+                'verbose': hp.choice('verbose', [1])
+            }
+
+}
+
+
+if __name__ == '__main__':
+    curr_dir = Path(__file__)
+    home_dir = curr_dir.parent.parent.parent
+    hyperparameters_path = Path(home_dir.as_posix()+'/models/hyperparameters.pkl')
+    pickle.dump(hyperparameters, open(hyperparameters_path, 'wb'))
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,40 +2,41 @@
 import pandas as pd 
 import numpy as np 
 import click
-import logging
 from pathlib import Path
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor,GradientBoostingRegressor
 from xgboost import XGBRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, mean_absolute_percentage_error,mean_squared_log_error
+from dvclive import Live
 import yaml
 import pickle
+from hyperopt import tpe, hp, Trials, STATUS_OK, fmin, space_eval
+from hyperopt.pyll.base import scope
 
+#setting up logging.info configuration 
+from src.logger import logging
 
-#setting up logging configuration 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
-file_handler = logging.FileHandler('train_model.log')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-
-logger.info('Creating dataset for modelling')
+logging.info('Building a model')
 
 #class for building features from dataset
 class TrainModel:
+    def __init__(self, trainpath, testpath,model, feat, ohe_feat, hyperparams,output_path, home_dir):
 
-    def __init__(self, read_path,model, features, ohe_feat, home_dir, **kwargs):
-
-        self.read_path = read_path
-        self.features = features
         self.model_cat = model
         self.onehot_encode_feat = ohe_feat
+        self.trainpath = trainpath
+        self.testpath = testpath 
+        self.features = feat
+        self.hyperparams = hyperparams
+        self.output_path = output_path
         self.home_dir = home_dir
+        self.hyperopt_algo = tpe.suggest
+        self.hyperopt_max_eval = 2
+        self.trials = Trials()
         #logreg for logistic regression checking if model needs to be build is logistic regression
         if model == 'LinearRegression': 
 
@@ -57,117 +58,149 @@ class TrainModel:
         elif model == 'XtremeGradietBoost':
 
             self.model_instance = XGBRegressor
-
-        try:        
-            self.model = self.model_instance(**kwargs)   #model initialisation
-        
-        except Exception as e:     
-
-            logger.info(f'Model initialisation failed with error : {e}')   #custom expectation raising 
-
-    
+   
     def read_data(self):
+        self.df_train = pd.read_csv(self.trainpath)
+        self.df_test = pd.read_csv(self.testpath)
+    
+    def feature(self):
 
-        '''This function reads csv data from input path and stores it into a dataframe'''
+        self.df_train= self.df_train[self.features]
+        self.df_test = self.df_test[self.features]
+        self.x_train = self.df_train.drop(columns=['trip_duration'])
+        self.y_train = self.df_train['trip_duration']
+        self.x_test = self.df_test.drop(columns= ['trip_duration'])
+        self.y_test = self.df_test['trip_duration']
 
-        try:
-            # Read data from the provided CSV file
-            self.df = pd.read_csv(self.read_path)
-        except Exception as e:
-            # Log if reading fails
-            logger.info(f'Reading failed with error: {e}')
-        else:
-            # Log if reading is successful
-            logger.info('Read performed successfully')
-
-    def keep_features(self):
-
-        try:
-            self.df = self.df[self.features]
-
-        except Exception as e:
-            # Log if reading fails
-            logger.info(f'Feature selecting failed with error: {e}')
-        
-        else:
-            logger.info('feature has been selected successfully')
-
-    def pipeline(self):
+    def ohe(self):
 
         '''This function initializes a pipeline'''
-        
-        try:
-            #initialing a one hot encoder to encode few features
-            ohe = OneHotEncoder(handle_unknown='ignore')
+        #initialing a one hot encoder to encode few features
+        ohe = OneHotEncoder(handle_unknown='ignore')
 
-            #a column transformer to perform ohe on passed onehot encoding fatures and passing the remaining features as it is 
-            self.oh_trf = ColumnTransformer([('encode_ohe', ohe, self.onehot_encode_feat)],remainder='passthrough')
+        #a column transformer to perform ohe on passed onehot encoding fatures and passing the remaining features as it is 
+        self.oh_trf = ColumnTransformer([('encode_ohe', ohe, self.onehot_encode_feat)],remainder='passthrough')
 
-            #initializing a pipeline
-            self.model_pipeline = Pipeline([('preprocess', self.oh_trf), ('regressor', self.model)])
+    def objective(self,params):
 
-        except Exception as e:
+        self.model = self.model_instance(**params)
 
-            #logger if pipeline initialization fails 
-            logger.info(f'Pipeline initialisation failed with error : {e}')
+        self.pipeline = Pipeline([('preprocess', self.oh_trf), ('regressor', self.model)])
 
-        else:
+        self.pipeline.fit(self.x_train, self.y_train)
 
-            #log if everything goes fine
-            logger.info('Pipeline has been initialised successfully')
+        self.train_score = model_eval(self.pipeline, self.x_train, self.y_train)
+        self.test_score = model_eval(self.pipeline, self.x_test, self.y_test)
+
+        with Live(self.output_path,save_dvc_exp=True) as live:
+            live.log_params(params)
+
+            live.log_metric('train data/RMSE',self.train_score['Root Mean Square Error'])
+            live.log_metric('train data/RMSPE',self.train_score['Root Mean Square Percentage Error'])
+            live.log_metric('train data/MAE',self.train_score['Mean Absolute Error'])
+            live.log_metric('train data/R2_SCORE',self.train_score['R2 Score'])
+
+            live.log_metric('test data/RMSE',self.test_score['Root Mean Square Error'])
+            live.log_metric('test data/RMSPE',self.test_score['Root Mean Square Percentage Error'])
+            live.log_metric('test data/MAE',self.test_score['Mean Absolute Error'])
+            live.log_metric('test data/R2_SCORE',self.test_score['R2 Score'])
     
-    def write_data(self):
+        return {'loss': self.test_score['Root Mean Square Error'], 'params':params, 'status':STATUS_OK}
 
+    def hyperopt_finetune(self):
+        
+        
+        self.best = fmin(fn=self.objective, 
+                    space=self.hyperparams, 
+                    algo=self.hyperopt_algo, 
+                    max_evals=self.hyperopt_max_eval, 
+                    trials=self.trials)
+        
+        self.best_hyperparams = space_eval(self.hyperparams, self.best)
+        self.model = self.model_instance(**self.best_hyperparams)
+        self.pipeline.fit(self.x_train, self.y_train)
+
+    def write_data(self):
         '''This function writes the data into destination folder'''
 
         try:
             # saving a binary model in models folder
-            filename = '/models/trained_pipeline.pkl'
-            pickle.dump(self.model_pipeline, open(Path(str(self.home_dir)+filename),'wb'))
+            modelfilename = '/models/trained_models/' + self.model_cat +'_pipeline.pkl'
+            pickle.dump(self.pipeline, open(Path(str(self.home_dir)+modelfilename),'wb'))
+
+            ohe_step = self.pipeline.named_steps['preprocess']
+            
+            df_train_transform  = ohe_step.transform(self.df_train)
+            df_test_transform  = ohe_step.transform(self.df_test)
+            
+            feature_names = ohe_step.get_feature_names_out()
+
+            df_train_transform = pd.DataFrame(df_train_transform, columns=feature_names)
+            
+            df_test_transform = pd.DataFrame(df_test_transform, columns=feature_names)
+
+            traindatafilename = '/data/processed/train_data_model.csv'
+            testdatafilename = '/data/processed/test_data_model.csv'
+
+            traindatafilename =Path(str(self.home_dir)+traindatafilename)
+            testdatafilename =Path(str(self.home_dir)+testdatafilename)
+            
+            df_train_transform.to_csv(traindatafilename)
+            df_test_transform.to_csv(testdatafilename)
+
+            
             
         except Exception as e:
             
             # Log if error in saving a model
-            logger.info(f'Model saving has been failed with error: {e}')
+            print(f'Model saving has been failed with error: {e}')
         
         else:
             # Log if model saved successfully
-            logger.info('Model has been saved successfully')
+            print('Model has been saved successfully')
 
-    
-    def input_output_split(self):
+    def train_model(self):
+        self.read_data()
+        self.feature()
+        self.ohe()
+        self.hyperopt_finetune()
+        print(self.best_hyperparams)
+        self.write_data()
 
-        #seperating input output columns from the data
-        self.x_train = self.df.drop(columns=['trip_duration'])
-        self.y_train = self.df['trip_duration']
 
-    def pipeline_fit(self):
-        '''This function performs all the operation in sync from reading a data from path to training a model. Post initialing a object 
-        making a call to pipeline_fit function will perform all the operations till saving a model automatically'''
+def model_eval(model, x, y):
 
-        try:
-            #calling all the functions in sequence to train a model
-            self.read_data()
-            self.keep_features()
-            self.input_output_split()
-            self.pipeline()
-            self.model_pipeline.fit(self.x_train, self.y_train)
+    y_pred = model.predict(x)
+
+    mse = round(mean_squared_error(y,y_pred),2)
         
-        except Exception as e:
-
-            #log if model training is failed 
-            logger.info(f'Pipeline fit on train data has been failed with error : {e}')
-
-        else:
-            
-            #logs if mopdel training has been performed successfully 
-            logger.info('Pipeline has been fitted successfully')
-            self.write_data()
-            return self.model_pipeline
+        #root mean `square error
+    rmse = round(np.sqrt(mean_squared_error(y,y_pred)),2)
+    
+    #mean absolute error
+    mae = round(mean_absolute_error(y,y_pred),2)
+    
+    #root mean square percentage error
+    rmspe = round(np.sqrt(np.sum(np.power(((y-y_pred)/y),2))/len(y)),3)
+    
+    #r2_score
+    r2 = round(r2_score(y,y_pred),2)
+    
+    #adjusted_r2_score
+    adjr2 = round(1-(1-r2_score(y,y_pred))*((x.shape[0]-1)/(x.shape[0]-x.shape[1]-1)),2)
+    
+    #dictionary storing all these testing score and this will be the returning value of function
+    score_dict = {'Mean Square Error':mse, 'Root Mean Square Error':rmse,
+                    'Mean Absolute Error':mae,'Root Mean Square Percentage Error':rmspe,'R2 Score':r2,
+                    'Adjusted R2 Score': adjr2 }
+    
+    return score_dict
 
 @click.command()
-@click.argument('input_filepath', type=click.Path())
-def main(input_filepath):
+@click.argument('traininput_filepath', type=click.Path())
+@click.argument('testinput_filepath', type=click.Path())
+@click.argument('hypparam_filepath', type=click.Path())
+def main(traininput_filepath, testinput_filepath,hypparam_filepath ):
 
     """ This script reads a train data from input folder and trains a model and store that model ito ../models folder.
     """
@@ -175,7 +208,10 @@ def main(input_filepath):
     curr_dir = Path(__file__)
     home_dir = curr_dir.parent.parent.parent
     data_dir = Path(home_dir.as_posix() + '/data')
-    input_path = Path(data_dir.as_posix() + input_filepath)
+    traininput_path = Path(data_dir.as_posix() + traininput_filepath)
+    testinput_path = Path(data_dir.as_posix() + testinput_filepath)
+    output_path = home_dir.as_posix() + '/dvclive'
+    hypparam_path = Path(home_dir.as_posix() + hypparam_filepath)
     params_path = Path(home_dir.as_posix()+'/params.yaml')
 
     #loading parameters of train model from params.yaml file 
@@ -183,13 +219,25 @@ def main(input_filepath):
     model= model_params['model']
     features = model_params['features']
     ohe = model_params['ohe_features']
-    hyperparams = yaml.safe_load(open(params_path))['train_model']['hyperparameters']
 
+    #loading a hyperparameter file 
+    hyperparams = pickle.load(open(hypparam_path, 'rb'))[model]
+    # df_train = pd.read_csv(traininput_path)
+    
+    # df_test = pd.read_csv(testinput_path)
+
+    # df_train = df_train[features]
+
+    # df_test = df_test[features]
+
+    # logging.info('call to class')
+    
     #initating a object from TrainModel class
-    model = TrainModel(input_path, model,features, ohe, home_dir,**hyperparams)
+    trf = TrainModel(traininput_path, testinput_path,model, features, ohe, hyperparams, output_path, home_dir)
 
-    #function call to fit and save a model 
-    model.pipeline_fit()
+    trf.train_model()
+
+    
 
 if __name__== "__main__":
     #call to a main function 
