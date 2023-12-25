d2f82a57b2bff31c115df6d7e5d7b8d3b2b26c0a
gupta4327 <gupta4327@gmail.com>
2023-12-25 17:32:24 +0530

    reinitialized dvc and added seperate files to dvc

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,12 +1,17 @@
+#importing necessary libraries
 import pandas as pd
 import pickle
 from src.features.build_features import BuildFeatures
 from pathlib import Path
 import yaml
+from src.logger import infologger
 
+#class designed to do production predictions
 class TripDurationPredictor:
 
     def __init__(self):
+
+        #initialising all necessary paths and parameters 
         self.curr_dir = Path(__file__)
         self.home_dir = self.curr_dir.parent.parent.parent
         self.model_path = Path(str(self.home_dir) + '/models/bestmodel.pkl')
@@ -16,30 +21,38 @@ class TripDurationPredictor:
 
     def dict_to_df(self,dict):
         
-        return pd.DataFrame(dict, index=[0])
+        #converting recieved input dictionary to dataframe
+        return pd.DataFrame(dict)
 
-    def loadmodel(self):
-        self.model = pickle.load(open(self.model_path, 'rb'))
-
+   
     def buildfeatures(self):
+
+        #building necessary features required for prediction
         feat = BuildFeatures()
         self.df = feat.build(self.df)
         self.df = self.df[self.features]
         
     def model_load(self):
+
+        #loading a binary model through pickle
         self.model = pickle.load(open(self.model_path, 'rb'))
 
     def predict_duration(self,dict):
 
-        self.df = self.dict_to_df(dict)
-        self.df['pickup_datetime'] = pd.to_datetime(self.df['pickup_datetime'])
-        self.buildfeatures()
-        self.model_load()
-        pred = self.model.predict(self.df)
-        return pred
+        # function that performs all other function together and returns a predicted output
+        try:
+            self.df = self.dict_to_df(dict)
+            self.buildfeatures()
+            self.model_load()
+            pred = self.model.predict(self.df)
+        except Exception as e:
+             infologger.info(f'Prediction has been failed because of error : {e}')
+        else:
+            return pred
     
 if __name__ == "__main__":
 
+        #dummy example if the main file has been ran directly 
         t = TripDurationPredictor()
         dict = {'vendor_id':[2], 'pickup_latitude':[40.767936706542969], 'pickup_longitude':[-73.982154846191406],
                 'dropoff_latitude':[40.765602111816406], 'dropoff_longitude':[-73.964630126953125], 
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+#importing necessary libraries
 from src.models.predict_model import TripDurationPredictor
 from src.models.train_model import model_eval
 import boto3  # pip install boto3
@@ -6,43 +7,59 @@ from pathlib import Path
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import numpy as np
 import matplotlib.pyplot as plt
+from src.logger import infologger
+
+#class to implement CI that will run predictor on test data and saves the scoring metrics graph
 
 class CI_test:
     def __init__(self):
+        
+        #trying to connect to s3
         try:
             s3 = boto3.client("s3")
 
 
             s3.download_file(
-                Bucket="nyctrip-bucket", Key="test_data.csv", Filename="data/interim/test_data.csv"
+                Bucket="nyctrip-bucket", Key="test_data.csv", Filename=Path("data/interim/test_data.csv")
             )
 
             s3.download_file(
-                Bucket="nyctrip-bucket", Key="bestmodel.pkl", Filename="models/bestmodel.pkl"
+                Bucket="nyctrip-bucket", Key="bestmodel.pkl", Filename=Path("models/bestmodel.pkl")
             )
 
         except Exception as e:
-            print(e)
+
+            #logger if connection with S3 fails
+            infologger.info(f'Not able to Connect to S3 :  {e}')
 
         else:
+
+            #initialize parameters and path if connection and download of data from s3 is successful
+
             self.read_path = Path(r'C:\Users\Aman Gupta\test\nyc_taxi_trip_duration_predictor\data\interim\test_data.csv')
             self.model_path = Path(r'C:\Users\Aman Gupta\test\nyc_taxi_trip_duration_predictor\models\bestmodel.pkl')
             self.df = pd.read_csv(self.read_path)
             self.x = self.df.drop(columns=['trip_duration'])
             self.y = self.df['trip_duration']
-            self.y_pred =[]
 
     def predict(self):    
+        
+        #initializing a object of predictor class
         predictor = TripDurationPredictor()
-        listofdict = self.x.to_dict("records")
-        for dct in listofdict:
-            self.y_pred.append(predictor.predict_duration(dct))
+        
+        #initializing a empty dictionary to get a dictionary of input vars
+        rec_dict = {}
 
+        #populating dictionary with input data 
+        for col in self.x.columns:
+            rec_dict[col] = list(self.x[col])
+
+        #generating predictions
+        self.y_pred = predictor.predict_duration(rec_dict)
+          
     def score(self):
 
-        #mean square error
-        mse = round(mean_squared_error(self.y,self.y_pred),2)
-            
+        
         #root mean `square error
         rmse = round(np.sqrt(mean_squared_error(self.y,self.y_pred)),2)
         
@@ -50,18 +67,16 @@ class CI_test:
         mae = round(mean_absolute_error(self.y,self.y_pred),2)
         
         #root mean square percentage error
-        rmspe = round(np.sqrt(np.sum(np.power(((self.y-self.y_pred)/self.y),2))/len(self.y)),3)
+        rmspe = round(np.sqrt(np.sum(np.power(((self.y-self.y_pred)/self.y),2))/len(self.y))*100,3)
         
         #r2_score
-        r2 = round(r2_score(self.y,self.y_pred),2)
+        r2 = round(r2_score(self.y,self.y_pred)*100,2)
         
-        #adjusted_r2_score
-        adjr2 = round(1-(1-r2_score(self.y,self.y_pred))*((self.x.shape[0]-1)/(self.x.shape[0]-self.x.shape[1]-1)),2)
         
         #dictionarself.y storing all these testing score and this will be the returning value of function
-        self.score_dict = {'Mean Square Error':mse, 'Root Mean Square Error':rmse,
-                        'Mean Absolute Error':mae,'Root Mean Square Percentage Error':rmspe,'R2 Score':r2,
-                        'Adjusted R2 Score': adjr2 }
+        self.score_dict = { 'Root Mean Square Error':rmse,
+                        'Mean Absolute Error':mae,'Root Mean Square Percentage Error':rmspe,'R2 Score':r2
+                        }
         
         return self.score_dict
 
@@ -72,20 +87,22 @@ class CI_test:
             self.score()
             fig, ax = plt.subplots()
 
-            ax.bar(list(self.score_dict.keys()), list(self.score_dict.keys()))
+            ax.bar(list(self.score_dict.keys()), list(self.score_dict.values()))
 
             ax.set_ylabel('Score')
             ax.set_xlabel('Metrices')
             ax.set_title('Different Scoring Metrices for model')
-           
+            plt.xticks(rotation = 'vertical')
             plt.savefig('metrices_bars.png', bbox_inches='tight')
 
 
         except Exception as e:
-            self.message = 'Error in plotting and predicting : ' + e
+            self.message = 'Error in plotting and predicting : ' + str(e)
+            infologger.info(self.message)
 
         else:
             self.message = 'Plotted successful'
+            infologger.info(self.message)
 
 if __name__ == '__main__':
     tst = CI_test()
